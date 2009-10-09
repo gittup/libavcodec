gittup/libavcodec

commit 347643b01bf54e95541b9c92282f410518ab0903

AAC: Add channel layout support for channel configuration as defined in the specification
git-svn-id: svn://svn.ffmpeg.org/ffmpeg/trunk/libavcodec@20067 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

aac.c
@@ -104,7 +104,6 @@ static VLC vlc_spectral[11];
 
 static ChannelElement *get_che(AACContext *ac, int type, int elem_id)
 {
-    static const int8_t tags_per_config[16] = { 0, 1, 1, 2, 3, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0 };
     if (ac->tag_che_map[type][elem_id]) {
         return ac->tag_che_map[type][elem_id];
     }
@@ -171,6 +170,26 @@ static int output_configure(AACContext *ac,
 
     memcpy(che_pos, new_che_pos, 4 * MAX_ELEM_ID * sizeof(new_che_pos[0][0]));
 
+    if (channel_config) {
+        for (i = 0; i < tags_per_config[channel_config]; i++) {
+            const int id = aac_channel_layout_map[channel_config - 1][i][1];
+            type         = aac_channel_layout_map[channel_config - 1][i][0];
+
+            if (!ac->che[type][id] && !(ac->che[type][id] = av_mallocz(sizeof(ChannelElement))))
+                return AVERROR(ENOMEM);
+
+            if (type != TYPE_CCE) {
+                ac->output_data[channels++] = ac->che[type][id]->ch[0].ret;
+                if (type == TYPE_CPE)
+                    ac->output_data[channels++] = ac->che[type][id]->ch[1].ret;
+            }
+        }
+
+        memset(ac->tag_che_map, 0,       4 * MAX_ELEM_ID * sizeof(ac->che[0][0]));
+        ac->tags_mapped = 0;
+
+        avctx->channel_layout = aac_channel_layout[channel_config - 1];
+    } else {
     /* Allocate or free elements depending on if they are in the
      * current program configuration.
      *
@@ -196,12 +215,10 @@ static int output_configure(AACContext *ac,
         }
     }
 
-    if (channel_config) {
-        memset(ac->tag_che_map, 0,       4 * MAX_ELEM_ID * sizeof(ac->che[0][0]));
-        ac->tags_mapped = 0;
-    } else {
         memcpy(ac->tag_che_map, ac->che, 4 * MAX_ELEM_ID * sizeof(ac->che[0][0]));
         ac->tags_mapped = 4 * MAX_ELEM_ID;
+
+        avctx->channel_layout = 0;
     }
 
     avctx->channels = channels;
@@ -1802,4 +1819,5 @@ AVCodec aac_decoder = {
     .sample_fmts = (const enum SampleFormat[]) {
         SAMPLE_FMT_S16,SAMPLE_FMT_NONE
     },
+    .channel_layouts = aac_channel_layout,
 };

aacdectab.h
@@ -69,4 +69,27 @@ static const float * const tns_tmp2_map[4] = {
 };
 // @}
 
+static const int8_t tags_per_config[16] = { 0, 1, 1, 2, 3, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+static const uint8_t aac_channel_layout_map[7][5][2] = {
+    { { TYPE_SCE, 0 }, },
+    { { TYPE_CPE, 0 }, },
+    { { TYPE_CPE, 0 }, { TYPE_SCE, 0 }, },
+    { { TYPE_CPE, 0 }, { TYPE_SCE, 0 }, { TYPE_SCE, 1 }, },
+    { { TYPE_CPE, 0 }, { TYPE_SCE, 0 }, { TYPE_CPE, 1 }, },
+    { { TYPE_CPE, 0 }, { TYPE_SCE, 0 }, { TYPE_LFE, 0 }, { TYPE_CPE, 1 }, },
+    { { TYPE_CPE, 0 }, { TYPE_SCE, 0 }, { TYPE_LFE, 0 }, { TYPE_CPE, 2 }, { TYPE_CPE, 1 }, },
+};
+
+static const int64_t aac_channel_layout[8] = {
+    CH_LAYOUT_MONO,
+    CH_LAYOUT_STEREO,
+    CH_LAYOUT_SURROUND,
+    CH_LAYOUT_4POINT0,
+    CH_LAYOUT_5POINT0_BACK,
+    CH_LAYOUT_5POINT1_BACK,
+    CH_LAYOUT_7POINT1_WIDE,
+    0,
+};
+
 #endif /* AVCODEC_AACDECTAB_H */

commit 01b02856e51eab3f0e56ad7f9004961450e9e045

AAC: Fix regression introduced in r20067 where ADTS files would always be signalled as having a channel configuration of 1 in output_configure(). Previously this didn't matter but it does now.
git-svn-id: svn://svn.ffmpeg.org/ffmpeg/trunk/libavcodec@20193 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

aac.c
@@ -1682,7 +1682,7 @@ static int parse_adts_frame_header(AACContext *ac, GetBitContext *gb)
             ac->m4ac.chan_config = hdr_info.chan_config;
             if (set_default_channel_config(ac, new_che_pos, hdr_info.chan_config))
                 return -7;
-            if (output_configure(ac, ac->che_pos, new_che_pos, 1))
+            if (output_configure(ac, ac->che_pos, new_che_pos, hdr_info.chan_config))
                 return -7;
         }
         ac->m4ac.sample_rate     = hdr_info.sample_rate;